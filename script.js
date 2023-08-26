e19a2090d9aaf6128ed29856039557c1b081f226
Sohaib Azhar <108861782+Sohaib-Azhar010@users.noreply.github.com>
2023-08-27 02:48:54 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+let string="";
+let button=document.querySelectorAll('.button');
+Array.from(button).forEach((button)=>{
+    button.addEventListener('click',(e)=>{
+        if(e.target.innerHTML=='='){
+            string=eval(string);
+            document.querySelector('input').value=string;
+        }
+        else if(e.target.innerHTML=='C'){
+            string="";
+            document.querySelector('input').value=string;
+        }
+        else{
+            string=string+e.target.innerHTML;
+            document.querySelector('input').value=string;
+        }
+    })
+})
